cff4cfef7ffe204a2b12e3c7612b68c1ca9538eb
FranciscoAscue <francisco.ascue131@gmail.com>
2021-03-04 22:09:15 -0500

    Version 1.2

--- preinstall.sh
+++ preinstall.sh
@@ -3,7 +3,7 @@
 ### Francisco Ascue Orosco
 ### gmail: francisco.ascue131@gmail.com
 
-normal=$'\e[0m'                           # (works better sometimes)
+normal=$'\e[0m'                          
 bold=$(tput bold)                         # make colors bold/bright
 red="$bold$(tput setaf 1)"                # bright red text
 green=$(tput setaf 2)                     # dim green text
@@ -19,45 +19,64 @@ gray=$(tput setaf 7)                      # dim white text
 darkgray="$bold"$(tput setaf 0)           # bold black = dark gray text
 white="$bold$gray"                        # bright white text
 
+
 if [ $UID -ne 0 ]
 then
-    sudo /bin/sh $0 $* && exit $?
-    echo "${red}Ejecutar el script con: sudo bash preinstall.sh${normal}"
-    exit 1
+	sudo /bin/bash $0 $* && exit $?
+	echo "${red}Se ha producido un error al ejecutar el script!${normal} ${cyan}Revisar las dependencias!${normal}"
+	exit 1
 fi
+
+
+echo "${green}........................................................................................................................................"
+echo "........................................................................................................................................"
+echo "........................................................................................................................................"
+echo ".......................................................${red}Instalación inicio: `date`${normal}${green}................................."
+echo "........................................................................................................................................"
+echo "........................................................................................................................................"
+echo "........................................................................................................................................${normal}"
+
+####### Inicio de actualizaciones
 
 if [ -x $(command -v apt) ]
 then
 	sudo apt update
-	echo "${purple}##############################################################################################"
-	echo "#################################${normal}${blue}INSTALANDO PRERREQUISITOS${normal}${purple}####################################"
-	echo "##############################################################################################"
+	echo "${green}.............................................................................................."
+	echo ".............................................................................................."
+	echo ".................................${normal}${darkcyan}INSTALANDO PRERREQUISITOS${normal}${green}..................................."
+	echo ".............................................................................................."
+	echo "..............................................................................................${normal}"
 	sudo apt-get install git wget curl
 	sudo apt-get install python3 python3-pyqt5.qtsvg python3-pyqt5 python3-pyqt5.qtsvg python3-pip
 
 	if [ -x $(command -v pip3) ]
 	then
+		set -e 
+		###
+		echo "${darkblue}"
 		sudo -H pip3 install --upgrade pip
-		echo "##############################################################################################"
-		echo "###################################${normal}${cyan} INSTALANDO JUPYTER ${normal}${purple}#######################################"
-		echo "##############################################################################################"
+		echo "${darkcyan}.............................................................................................."
+		echo "...................................${normal}${cyan} INSTALANDO JUPYTER ${normal}${darkcyan}......................................."
+		echo "...............................................................................................${normal}"
 		pip3 install ipykernel
 		pip3 install jupyter
 	fi
 
-	echo "------------------------------------${yellow}Instalando Kernel de BASH${purple}--------------------------------"
+	set -e
+
+	echo "${green}------------------------------------${normal}${yellow}Instalando Kernel de BASH${green}--------------------------------"
 	sudo apt-get install libzmq3-dev libcurl4-openssl-dev libssl-dev jupyter-core jupyter-client
 	pip3 install bash_kernel
 	python3 -m bash_kernel.install
-	echo "-------------------------------${yellow}Instalando paquetes de compilacion${purple}----------------------------"
+	echo "-------------------------------${yellow}Instalando paquetes de compilacion${green}----------------------------"
 	sudo apt-get install gcc make-guile ubuntu-make make build-essential libexpat1-dev
 fi
 
 
 ### Instalacion de Programas bioinfo
 
-mkdir -P Programas/bin
-PGM="$HOME/Curso_trasncriptomica/Programas"
+mkdir -p Programas/bin
+PGM="$PWD/Programas"
 export PATH='${PGM}/bin:$PATH' >> ~/.bashrc
 
 cd $PGM
@@ -68,7 +87,7 @@ echo "--------------------------------------------------------------------------
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------------------------------------------"
-echo "------------------------------------${cyan}INSTALACION DE PROGRAMAS DE BIOINFORMATICA${purple}-----------------------------------"
+echo "------------------------------------${cyan}INSTALACION DE PROGRAMAS DE BIOINFORMATICA${green}-----------------------------------"
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------------------------------------------"
@@ -77,26 +96,42 @@ echo "--------------------------------------------------------------------------
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------------------------------------------"
+
+#####Programs Bioinfo
+set -e 
+
+sudo apt-get install fastqc cutadapt samtools subread stringtie sortmerna rna-star ncbi-entrez-direct
 
 
 if [ -x $(command -v curl) ]
 then
-	sudo apt install fastqc cutadapt samtools subread stringtie sortmerna rna-star
+	set -e
+
 	curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.6.6.tar.gz -o trim_galore.tar.gz
 	if [ -s "trim_galore.tar.gz" ]
 	then
 		tar xvzf trim_galore.tar.gz
 		ln -s $PWD/TrimGalore-0.6.6/trim_galore $PWD/bin/
 	fi
-
-	#Instalacion adicional
-
-	pip3 install gffutils	
-	pip3 install bcbio-gff	
-	pip3 install HTseq
-	pip3 install multiqc
-	sudo apt install ncbi-entrez-direct
 fi
+
+
+#Instalacion adicional
+
+set -e 
+
+pip3 install gffutils	
+pip3 install bcbio-gff	
+pip3 install HTseq
+pip3 install multiqc
+
+echo "........................................................................................................................................"
+echo "........................................................................................................................................"
+echo "........................................................................................................................................"
+echo ".......................................................${blue}Instalación fin: `date` ${normal}${green}.................................."
+echo "........................................................................................................................................"
+echo "........................................................................................................................................"
+echo "........................................................................................................................................"
 
 echo "${cyan}"
 echo '''  __ _       _     _'''
@@ -105,4 +140,4 @@ echo '''| |_ _ _ __  _ ___| |_'''
 echo '''|  _| |  _ \| / __|  _ \'''
 echo '''| | | | | | | \__ \ | | |'''
 echo '''|_| |_|_| |_|_|___/_| |_|'''
-
+echo "Los programas se han instalado correctamente"
