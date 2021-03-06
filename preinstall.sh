aee029f6448998603d993ad63e4a479300cd6992
FranciscoAscue <francisco.ascue131@gmail.com>
2021-03-06 15:19:19 -0500

    version 1.3

--- preinstall.sh
+++ preinstall.sh
@@ -101,6 +101,7 @@ echo "--------------------------------------------------------------------------
 
 if [ -x $(command -v apt-get) ]
 then
+	sudo apt update
 	sudo apt-get install fastqc cutadapt samtools subread stringtie sortmerna rna-star ncbi-entrez-direct -y
 fi
 
