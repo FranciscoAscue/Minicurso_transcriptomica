c6de8ddb82f5039262e53f00a1c071d4082c70cf
FranciscoAscue <francisco.ascue131@gmail.com>
2021-04-17 23:43:43 -0500

    RNA-seq Files

---
+++ RNA_seq/reads/d.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+for i in 04 05 06 07 08 09 10
+do
+	mkdir -p SRR8495${i}
+done
+
+
+
+
+
+
+
+
+
+
+
+
+# wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt
+
