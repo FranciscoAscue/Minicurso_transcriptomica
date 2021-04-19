a82bd24040a63c41aac7bd0fc3057d2752f8a76a
FranciscoAscue <57104899+FranciscoAscue@users.noreply.github.com>
2021-04-18 22:36:55 -0500

    Update Bowtie2.bash

--- Bowtie2.bash
+++ Bowtie2.bash
@@ -4,12 +4,12 @@
 
 ###CONSTANTS
 
-WD="/mnt/disco2/fascue/Cporecellus"
-REF="${WD}/data/reference/index/cavpor3"
+WD="$HOME/Minicurso_transcriptomica"
+REF="${WD}/data/reference/index/indexname"
 RES="${WD}/results"
-READS="${WD}/data/reads/------"
-r1="${READS}/----_1.fq"
-r2="${READS}/----_2.fq"
+READS="${WD}/data/reads/"
+r1="${READS}/fastqfile_1.fq"
+r2="${READS}/fastqfile_2.fq"
 OD="${RES}/map"
 
 ###EXECUTION
@@ -18,12 +18,12 @@ echo "started at `date`"
 echo "mkdir -p ${OD}"
 mkdir -p ${OD}
 
-echo "bowtie -end-to-end -I 0 -X 1000 -p 30 -x ${REF} -1 $r1 -2$r2 -S ${OD}/cavtsc.sam"
-bowtie -end-to-end -I 0 -X 1000 -p 30 -x ${REF} -1 $r1 -2$r2 -S ${OD}/cavtsc.sam
-echo "samtools view -u@ 10 ${OD}/cavtsc.sam | samtools sort -@ 10 -o ${OD}/cavtsc.sorted.bam -"
-samtools view -u@ 8 ${OD}/cavtsc.sam | samtools sort -@ 40 -o ${OD}/cavtsc.sorted.bam -
-echo "samtools index ${OD/cavtsc.sorted.bam"
-samtools index ${OD}/cavtsc.sorted.bam
+echo "bowtie -end-to-end -I 0 -X 1000 -p 30 -x ${REF} -1 $r1 -2$r2 -S ${OD}/output.sam"
+bowtie -end-to-end -I 0 -X 1000 -p 30 -x ${REF} -1 $r1 -2$r2 -S ${OD}/output.sam
+echo "samtools view -u@ 10 ${OD}/output.sam | samtools sort -@ 10 -o ${OD}/output.sorted.bam -"
+samtools view -u@ 8 ${OD}/output.sam | samtools sort -@ 40 -o ${OD}/output.sorted.bam -
+echo "samtools index ${OD/output.sorted.bam"
+samtools index ${OD}/output.sorted.bam
 
 echo "Finished at `date`"
 
